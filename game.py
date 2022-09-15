6fd2915ad498dcb413e221bbde02edf0f0c521c3
VI269 <111111107+VI269@users.noreply.github.com>
2022-09-15 12:21:59 +0800

    data

--- game.py
+++ game.py
@@ -1,8 +1,20 @@
 import random
 import colorama
 from colorama import Fore, Back, Style
-
-X = "idk"
+import os
 
 colorama.init()
 
+def prompt(args=''):
+    inp = input(f"{Fore.YELLOW + args}>{Fore.LIGHTBLUE_EX}{Style.BRIGHT} ")
+    return inp
+
+def log(text):
+    print(f"{Fore.LIGHTGREEN_EX}{text}")
+
+os.system('clear')
+
+log('Welcome To Far Land Town Located at the Very North of Your Map. For a Guide on How to Play Enter "help" as the Command When The Prompt Shows Up.')
+
+while True:
+    command = prompt()
