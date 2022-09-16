3d20d63c3a46b2f881c7bb5400a465659831d6e8
VI269 <111111107+VI269@users.noreply.github.com>
2022-09-16 23:41:14 +0800

    CHANGES

--- language-versions/python-ver/app.py
+++ language-versions/python-ver/app.py
@@ -3,24 +3,29 @@ import colorama
 from colorama import Fore, Back, Style
 import os
 import json
+import pathlib
 
 colorama.init()
 
+directory = pathlib.Path(__file__).parent.resolve()
+
 dataTemp = {
     "far_land_town": {
-        "places": {
-            "curPos": None,
+        "town_square": {
+            "curPos": False,
+            "explored": False,
+            "desc": "A Fountain lies in front of you. Some people walk around the green environment with snowy mountains visible accross the horizon. A sign stands before you and a poster is on your right."
+        },
+        "busy_street": {
+            "curPos": False,
+            "explored": False,
+            "desc": "The street you are looking at is packed with people, traders and high ranked workers. A sign is in front of you with a man in black staring at you.",
+            "interactables": ["sign", "man_in_black", ""]
         }
-    },
-    "forests": {
-
-    },
-    "emperial_city": {
-
     }
 }
 
-save = {}
+saveData = {}
 
 def prompt(args=''):
     inp = input(f"{Fore.YELLOW + args}>{Fore.LIGHTBLUE_EX}{Style.BRIGHT} ")
@@ -35,12 +40,20 @@ log('Welcome To Far Land Town Located at the Very North of Your Map. For a Guide
 
 while True:
     command = prompt().lower()
-    if command.split(' ')[0] == "save":
-        args = command.split(' ')[1]
+    if command.split(' ')[0] == "load":
         try:
-            with open(f'/saves/{args}.json', 'r') as f:
+            args = command.split(' ')[1]
+        except:
+            print("You haven't put a save name, enter the save name to load it")
+        try:
+            with open(f'{directory}/saves/{args}.json', 'r') as f:
                 log("Loaded Save")
-                save = json.dump(f)
-
+                saveData = f.read()
         except FileNotFoundError:
             log("Save Not Found")
+    elif command.split(' ')[0] == 'look':
+        try:
+            args = command.split(' ')[1]
+        except:
+            pass
+        
